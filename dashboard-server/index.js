e75881038a4b96886009f3d4741cf344bb8ecff7
mudassir <mudassirahmedbashir@hotmail.co.uk>
2019-11-17 06:08:10 +0000

    Add server that deals with emissions

---
+++ dashboard-server/index.js
@@ -0,0 +1,37 @@
+// run using node index.js
+
+var config = require("../vue-app/src/assets/project-config.json");
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  res.send("<h1>Hello world</h1>");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+
+  socket.on("increment-temp", function() {
+    console.log("Request to increment temp");
+    io.emit("increment-temp");
+  });
+
+  socket.on("decrement-temp", function() {
+    console.log("Request to decrement temp");
+    io.emit("decrement-temp");
+  });
+
+  socket.on("toggle-auto-pilot", function() {
+    console.log("Request to toggle-auto-pilot");
+    io.emit("toggle-auto-pilot");
+  });
+});
+
+http.listen(config.PORT, function() {
+  console.log(`listening on *:${config.PORT}`);
+});
